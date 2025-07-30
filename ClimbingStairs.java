19f717084496e31c2a880e4cdacb9a25ecf35e98
Greeshma Shettigar <shettigargreeshma@gmail.com>
2025-07-30 18:20:37 +0530

    Refactor Climb method to initialize memoization array and improve base case handling

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,8 +1,10 @@
+/*Count ways to reach the nth stair .The person can climb either 1  stair or 2 stair at a time */
+//using memeoization
 class ClimbingStairs {
     public int Climb(int n, int f[]){
-        f[1] = 1;
-        f[2]=2;
-        if(f[n] != 0) return f[n];
+        if(n==0) return 1; // Base case: 1 way to stay at the ground
+        if(n < 0) return 0; // No way to climb negative stairs
+        if(f[n] != -1) return f[n];
        /*for(int i=3;i<n;i++){
        f[i]=f[i-1]+f[i-2]
        } */ 
@@ -19,3 +21,18 @@ class ClimbingStairs {
         // Output: Number of ways to climb 10 stairs: 89    
    }
     }   
+
+    // using tabulation
+    /*public int Climb(int n){
+    int[] dp = new int[n + 1];
+    dp[0] = 1; // Base case: 1 way to stay at the ground
+    for(int i=1;i<=n;i++){
+      if(i==1){
+      dp{i] = dp[i - 1]; // Only one way to climb 1 stair
+      } else {
+        dp[i] = dp[i - 1] + dp[i - 2]; // Sum of ways to climb (i-1) and (i-2) stairs
+        }
+      }
+    return dp[n];
+    }
+    */
