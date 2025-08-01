0295d1d3f924b594996184660de174775931c39a
Greeshma Shettigar <shettigargreeshma@gmail.com>
2025-08-02 02:32:29 +0530

    Refactor whitespace in Longest Common Subsequence implementation for improved readability

--- longectCommonSubsequence.java
+++ longectCommonSubsequence.java
@@ -7,6 +7,7 @@ public class longectCommonSubsequence {
     public static int commonSubsequence(String s1, String s2){
         int n=s1.length();
         int m=s2.length();
+        
         int[][] dp = new int[n+1][m+1];
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
@@ -17,11 +18,13 @@ public class longectCommonSubsequence {
             for(int j=1;j<=m;j++){
                 if(s1.charAt(i-1)==s2.charAt(j-1)){
                     dp[i][j]=dp[i-1][j-1]+1;
+                
                 }else{
                     dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
+        
         return dp[n][m]; // Return the length of the longest common subsequence
     }
     public static void main(String[] args) {
